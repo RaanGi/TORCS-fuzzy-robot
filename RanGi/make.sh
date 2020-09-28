6fbf2dc1971d8230f1a8e29b7d9bde550825d9ab
Marcos <markos@rangi-zorin-x-air13.3>
2020-09-28 19:32:31 +0200

    Fuzzyengines was deleted after been built

--- RanGi/make.sh
+++ RanGi/make.sh
@@ -3,11 +3,12 @@
 fuzzylite='/src/drivers/RanGi/fuzzylite'		
 fuzzylite_lib='/src/drivers/RanGi/fuzzylite/release/bin'	
 fuzzylite=${TORCS_BASE}${fuzzylite}			# Fuzzylite source path
-fuzzlite_lib=${TORCS_BASE}${fuzzylite_lib}		# Fuzzylite lib path
+fuzzylite_lib=${TORCS_BASE}${fuzzylite_lib}		# Fuzzylite lib path
+
+rm -f fuzzyengines.o fuzzyengines
 
 g++ -o fuzzyengines fuzzyengines.cpp -I${fuzzylite} -L${fuzzylite_lib} -lfuzzylite-static -lrt -lpthread
 
 make install
 
 make clean
-rm -f fuzzyengines.o fuzzyengines
